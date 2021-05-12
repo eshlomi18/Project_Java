28afe6d5d69105b253e2b92eb09803083bd5afad
samuel verse <verse@g.jct.ac.il>
2021-05-13 00:29:04 +0300

    triangle tests ok

--- src/elements/SpotLight.java
+++ src/elements/SpotLight.java
@@ -3,25 +3,24 @@ package elements;
 import primitives.*;
 import primitives.Util;
 
-import static primitives.Util.alignZero;
-import static primitives.Util.isZero;
+import static primitives.Util.*;
 
 public class SpotLight extends PointLight {
-    private Vector direction;
+    private final Vector direction;
 
     public SpotLight(Color intensity, Point3D position, Vector direction) {
         super(intensity, position);
-        this.direction = direction;
+        this.direction = direction.normalized();
     }
 
     public SpotLight(Color intensity, Point3D position, Vector direction, double kC, double kL, double kQ) {
         super(intensity, position, kC, kL, kQ);
-        this.direction = direction;
+        this.direction = direction.normalized();
     }
 
     @Override
     public Color getIntensity(Point3D p) {
-        double factor = alignZero(Math.max(0, direction.dotProduct(getL(p))));
-        return super.getIntensity().scale(factor);
+        double cosTheta=direction.dotProduct(getL(p));
+        return super.getIntensity(p).scale(Math.max(0,cosTheta));
     }
 }
