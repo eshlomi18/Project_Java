a5166a010a8a041cf0d9a7a3c7bff765c9f344fb
eshlomi18 <eshlomi15@gmail.com>
2021-05-27 17:04:15 +0300

    part 1

--- unittests/renderer/ShadowTests.java
+++ unittests/renderer/ShadowTests.java
@@ -43,6 +43,94 @@ public class ShadowTests {
 		render.writeToImage();
 	}
 
+	@Test
+	public void sphereTriangleInitial1() throws MissingResourcesException, UnsupportedOperationException {
+		scene.geometries.add( //
+				new Sphere(60, new Point3D(0, 0, -200)) //
+						.setEmission(new Color(java.awt.Color.BLUE)) //
+						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(30)), //
+				new Triangle(new Point3D(-70, -40, 0), new Point3D(-40, -70, 0), new Point3D(-68, -68, -4)) //
+						.setEmission(new Color(java.awt.Color.BLUE)) //
+						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(30)) //
+		);
+		scene.lights.add( //
+				new SpotLight(new Color(400, 240, 0), new Point3D(-100, -100, 250), new Vector(1, 1, -3)) //
+						.setKl(1E-5).setKq(1.5E-7));
+
+		Render render = new Render(). //
+				setImageWriter(new ImageWriter("shadowSphereTriangleInitial1", 400, 400)) //
+				.setCamera(camera) //
+				.setRayTracer(new BasicRayTracer(scene));
+		render.renderImage();
+		render.writeToImage();
+	}
+
+	@Test
+	public void sphereTriangleInitial2() throws MissingResourcesException, UnsupportedOperationException {
+		scene.geometries.add( //
+				new Sphere(60, new Point3D(0, 0, -200)) //
+						.setEmission(new Color(java.awt.Color.BLUE)) //
+						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(30)), //
+				new Triangle(new Point3D(-70, -40, 0), new Point3D(-40, -70, 0), new Point3D(-68, -68, -4)) //
+						.setEmission(new Color(java.awt.Color.BLUE)) //
+						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(30)) //
+		);
+		scene.lights.add( //
+				new SpotLight(new Color(400, 240, 0), new Point3D(-100, -100, 150), new Vector(1, 1, -3)) //
+						.setKl(1E-5).setKq(1.5E-7));
+
+		Render render = new Render(). //
+				setImageWriter(new ImageWriter("shadowSphereTriangleInitial2", 400, 400)) //
+				.setCamera(camera) //
+				.setRayTracer(new BasicRayTracer(scene));
+		render.renderImage();
+		render.writeToImage();
+	}
+
+	@Test
+	public void sphereTriangleInitial3() throws MissingResourcesException, UnsupportedOperationException {
+		scene.geometries.add( //
+				new Sphere(60, new Point3D(0, 0, -200)) //
+						.setEmission(new Color(java.awt.Color.BLUE)) //
+						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(30)), //
+				new Triangle(new Point3D(-70, -40, 0), new Point3D(-40, -70, 0), new Point3D(-68, -68, -4)) //
+						.setEmission(new Color(java.awt.Color.BLUE)) //
+						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(30)) //
+		);
+		scene.lights.add( //
+				new SpotLight(new Color(400, 240, 0), new Point3D(-73, -73, 57), new Vector(1, 1, -3)) //
+						.setKl(1E-5).setKq(1.5E-7));
+
+		Render render = new Render(). //
+				setImageWriter(new ImageWriter("shadowSphereTriangleInitial3", 400, 400)) //
+				.setCamera(camera) //
+				.setRayTracer(new BasicRayTracer(scene));
+		render.renderImage();
+		render.writeToImage();
+	}
+
+	@Test
+	public void sphereTriangleInitial4() throws MissingResourcesException, UnsupportedOperationException {
+		scene.geometries.add( //
+				new Sphere(60, new Point3D(0, 0, -200)) //
+						.setEmission(new Color(java.awt.Color.BLUE)) //
+						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(30)), //
+				new Triangle(new Point3D(-47, -17, 23), new Point3D(-17, -47, 23), new Point3D(-45, -45, 19)) //
+						.setEmission(new Color(java.awt.Color.BLUE)) //
+						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(30)) //
+		);
+		scene.lights.add( //
+				new SpotLight(new Color(400, 240, 0), new Point3D(-100, -100, 270), new Vector(1, 1, -3)) //
+						.setKl(1E-5).setKq(1.5E-7));
+
+		Render render = new Render(). //
+				setImageWriter(new ImageWriter("shadowSphereTriangleInitial4", 400, 400)) //
+				.setCamera(camera) //
+				.setRayTracer(new BasicRayTracer(scene));
+		render.renderImage();
+		render.writeToImage();
+	}
+
 	/**
 	 * Produce a picture of a two triangles lighted by a spot light with a Sphere
 	 * producing a shading
